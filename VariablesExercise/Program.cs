acd03d31cd1428766b16c7d07942dda8b5bde6b6
Jonathan Meza <jonathansyear92@gmail.com>
2022-08-21 20:01:07 -0600

    initial commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declared and initialized a variable.
+            //camelCase for the variable name
+            //explicit typing
+            string dogName = "Ralf";
+            int dogAge = 10;
+            char nickName = 'R';
+            bool lovesHiking = true;
+            double tennisBalls = 32;
+            decimal weight = 70.3m;
+
+
+            //string interpolation with the $ AND { } around variables
+            Console.WriteLine($"My dog's name is {dogName}, he is {dogAge} years old and he has {tennisBalls} tennis balls and it is {lovesHiking} that he loves to hike, which keeps him at his weight of {weight} pounds.");
+          
+        }
+    }
+}
+
